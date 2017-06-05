69d9b49ce8d5a50ffe2580bc66b7504d46738b85
MayankSrivastav <mayanksri18@yahoo.in>
2017-06-05 22:26:17 +0530

    Create the App component

--- js/index.js
+++ js/index.js
@@ -1,3 +1,14 @@
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Random />
+        <Custom />
+      </div>      
+    ); 
+  }  
+}
+
 ReactDOM.render(
   <App />,
   document.getElementById("app")
