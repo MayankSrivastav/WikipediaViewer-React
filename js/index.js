dc8c1cf037f327cdc62f4b1ae2f8a06bdec5abb1
MayankSrivastav <mayanksri18@yahoo.in>
2017-06-10 11:15:33 +0530

    Written ajax call for results from wikipedia

--- js/index.js
+++ js/index.js
@@ -10,6 +10,25 @@ class SearchBar extends React.Component {
   handleClick() {       
     const url = "https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=" 
               + document.getElementById('search').value; 
+
+    $.ajax({
+      url: url,
+      dataType: 'jsonp',
+      success: (data) => {        
+        var res = data.query.pages;
+        var html = "";        
+        $.each(res, function(k, v) {
+          html += '<a href="' + "https://en.wikipedia.org/?curid=" + v.pageid + '"target="_blank"><article class="article">'
+            + '<header><h2>&#x2022; ' + v.title + '</h2><header>'
+            + '<p class="extract">'+ v.extract + '</p>'            
+            + '</article></a>';          
+        });        
+        this.setState({results: html});     
+      },
+      error: function(err) {
+        alert(err);
+      }
+    });
   }
 }
 
